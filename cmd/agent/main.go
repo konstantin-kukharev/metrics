edf92726777ea4d997f719ecde86d30a4815d19d
KK <konstantin.kukharev@gmail.com>
2024-12-03 18:08:06 +0300

    ? fixing test

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/konstantin-kukharev/metrics/cmd/agent/report"
 	"github.com/konstantin-kukharev/metrics/cmd/agent/service"
@@ -17,13 +19,15 @@ func main() {
 	r := report.NewRest(c)
 	srv := service.NewState(conf, s, r)
 
+	time.Sleep(time.Second * 1)
+
 	if err := srv.Run(); err != nil {
-		// fmt.Printf(
-		// 	"runninig agent\r\nreport on %s\r\nreport interval: %s sec.\r\npool interval: %s sec.\r\n",
-		// 	conf.GetServerAddress(),
-		// 	conf.GetReportInterval(),
-		// 	conf.GetPoolInterval(),
-		// )
+		fmt.Printf(
+			"runninig agent\r\nreport on %s\r\nreport interval: %s sec.\r\npool interval: %s sec.\r\n",
+			conf.GetServerAddress(),
+			conf.GetReportInterval(),
+			conf.GetPoolInterval(),
+		)
 
 		log.Fatal(err)
 	}
